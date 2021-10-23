e93c39c8f629c9717160d4ec4f8fa36d7f8a71eb
BlazerHeat <vghoghari82@gmail.com>
2021-10-23 22:46:03 +0530

    small bug fix

--- utils/antiauthcheck.js
+++ utils/antiauthcheck.js
@@ -1,4 +1,8 @@
 module.exports = (req, res, next) => {
+    if(req.path === '/logout'){
+        next();
+        return;
+    }
     if(req.user)
         res.status(302).redirect('/profile/'+req.user.id);
     else

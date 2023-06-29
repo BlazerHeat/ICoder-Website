4d93f0691a6afb3c883c402b15df1da40359f34c
BlazerHeat <vghoghari82@gmail.com>
2023-06-29 10:28:43 +0530

    Small update

--- database/connection.js
+++ database/connection.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 
 async function connect() {
-    await mongoose.connect(process.env.DatabaseURI,  (error) => {
-        if(error)
-            console.error(error);
-        else
-            console.log(chalk.green("Connected to Database!"));
-    });
+  await mongoose.connect(process.env.DATABASE_URL, (error) => {
+    if (error) console.error(error);
+    else console.log(chalk.green('Connected to Database!'));
+  });
 }
 
 module.exports.connect = connect;
--- utils/compiler.js
+++ utils/compiler.js
@@ -1,24 +1,23 @@
-const axios = require("axios");
-const chalk = require("chalk");
+const axios = require('axios');
+const chalk = require('chalk');
 const API_END_POINT = require('../config.json').JDoodle_API_ENDPOINT;
 
-module.exports = async (language, script, stdin = "") => {
-    let res = await axios({
-      url: API_END_POINT,
-      method: "POST",
-      timeout: 5000,
-      data: {
-        script,
-        language,
-        stdin,
-        clientId: process.env.ClientID,
-        clientSecret: process.env.ClientSecret,
-      },
-    }).catch(err => {
-        console.error(chalk.red(err.toString()));
-    });
+module.exports = async (language, script, stdin = '') => {
+  let res = await axios({
+    url: API_END_POINT,
+    method: 'POST',
+    timeout: 5000,
+    data: {
+      script,
+      language,
+      stdin,
+      clientId: process.env.JDOODLE_CLIENT_ID,
+      clientSecret: process.env.JDOODLE_CLIENT_SECRET,
+    },
+  }).catch((err) => {
+    console.error(chalk.red(err.toString()));
+  });
 
-    if(!res)
-        return { err: "No Response from API." };
-    return res.data;
+  if (!res) return { err: 'No Response from API.' };
+  return res.data;
 };
